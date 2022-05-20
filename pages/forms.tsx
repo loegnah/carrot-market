2cfb6462183bca8ad37641625f35093b06f41033
loegnah <loegnah@gmail.com>
2022-05-20 22:25:14 +0900

    [form] valiadation ph.1

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,28 +1,50 @@
 import type { NextPage } from "next";
+import type {
+  SubmitErrorHandler,
+  SubmitHandler,
+  FieldValues,
+} from "react-hook-form";
 import { useForm } from "react-hook-form";
 
-const Forms: NextPage = () => {
-  const { register, handleSubmit } = useForm();
+type LoginForm = {
+  username: string;
+  password: string;
+  email: string;
+};
 
-  const onValid = () => {
-    console.log("is valid");
+const Forms: NextPage = () => {
+  const { register, handleSubmit } = useForm<LoginForm>();
+
+  const onValid: SubmitHandler<LoginForm> = (data, event) => {
+    console.log(data);
+    console.log(event);
+  };
+
+  const onInValid: SubmitErrorHandler<LoginForm> = (errors, event) => {
+    console.log(errors);
+    console.log(event);
   };
 
   return (
-    <form onSubmit={handleSubmit(onValid)}>
+    <form onSubmit={handleSubmit(onValid, onInValid)}>
       <input
-        {...register("username", { required: true })}
+        {...register("username", {
+          required: "username is required",
+          minLength: {
+            value: 5,
+            message: "The username should me longer than 5",
+          },
+        })}
         type="text"
         placeholder="Username"
-        minLength={5}
       />
       <input
-        {...(register("email"), { required: true })}
+        {...register("email", { required: true })}
         type="email"
         placeholder="Email"
       />
       <input
-        {...(register("password"), { required: true })}
+        {...register("password", { required: true })}
         type="password"
         placeholder="Password"
       />
