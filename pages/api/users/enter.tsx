7dd44a360e014c887235e485c7b11684c5ce8cef
loegnah <loegnah@gmail.com>
2022-05-29 20:42:42 +0900

    [prisma] enter handler

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler from "@libs/server/withHandler";
+import client from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { email, phone } = req.body;
+  const payload = email ? { email } : { phone: +phone };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
   return res.status(200).end();
 }
 
