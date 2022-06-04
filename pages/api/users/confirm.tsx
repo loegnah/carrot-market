72d3918908714ee80a76d0a2158d9252870d90bf
loegnah <loegnah@gmail.com>
2022-06-04 20:59:31 +0900

    [Session] iron-session send cookie

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,14 +1,38 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 import type { NextApiRequest, NextApiResponse } from "next";
+
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
-  console.log(token);
+  const existToken = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  console.log(existToken);
+  if (!existToken) res.status(404).end();
+
+  req.session.user = {
+    id: existToken!.userId,
+  };
+  await req.session.save();
+
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password: "9759827459254702345972342342342342341111128458",
+});
