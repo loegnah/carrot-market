e7bcad6e450edaa1dadd8d92326fb99e7ea604da
loegnah <loegnah@gmail.com>
2022-06-06 00:54:47 +0900

    [API] Protect withHandler

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -1,23 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export type ResponseType = {
+type ResponseType = {
   ok: boolean;
   [key: string]: any;
 };
 
-export default function withHandler(
-  method: "GET" | "POST" | "DELETE",
-  fn: (req: NextApiRequest, res: NextApiResponse) => void
-) {
+type WithHandlerParams = {
+  method: "GET" | "POST" | "DELETE";
+  handler: (req: NextApiRequest, res: NextApiResponse) => void;
+  isPrivate?: boolean;
+};
+
+function withHandler({ method, handler, isPrivate = true }: WithHandlerParams) {
   return async function (req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== method) {
       return res.status(405).end();
     }
+    if (isPrivate && !req.session.user) {
+      return res.status(401).json({ ok: false, error: "Please log in." });
+    }
     try {
-      await fn(req, res);
+      await handler(req, res);
     } catch (error) {
       console.log(error);
       return res.status(500).json({ error });
     }
   };
 }
+
+export default withHandler;
+export type { ResponseType };
