e532a3699caf1bc113d87b6406b33ae06969f3b8
loegnah <loegnah@gmail.com>
2022-06-06 22:25:04 +0900

    Making prisma client not duplicately

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -1,3 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-export default new PrismaClient();
+declare global {
+  var client: PrismaClient | undefined;
+}
+
+if (!global.client) {
+  global.client = new PrismaClient();
+}
+
+export default global.client;
