95defeaa13b239ad9ab8c8192c6922cd0db00e24
loegnah <loegnah@gmail.com>
2022-06-06 22:09:49 +0900

    [hook] init SWR

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,21 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
+import useSWR from "swr";
 
 export default useUser;
 
 function useUser() {
-  const [user, setUser] = useState();
+  const { data, error } = useSWR("/api/users/me");
   const router = useRouter();
 
   useEffect(() => {
-    fetch("/api/users/me")
-      .then((response) => response.json())
-      .then((data) => {
-        if (!data.ok) {
-          return router.replace("/enter");
-        }
-        setUser(data.profile);
-      });
-  }, [router]);
-  return user;
+    if (data && !data.ok) {
+      router.replace("/enter");
+    }
+  }, [data, router]);
+
+  return { user: data?.profile, isLoading: !data && !error };
 }
