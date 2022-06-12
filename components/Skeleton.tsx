485de988940fe39a8d5525a5c3c591859fd29924
loegnah <loegnah@gmail.com>
2022-06-12 21:05:49 +0900

    [product] Make and Apply Skeleton

---
+++ components/Skeleton.tsx
@@ -0,0 +1,25 @@
+const SkeletonCard = () => {
+  return (
+    <>
+      <div className="flex w-full flex-1 flex-col items-center px-4">
+        <div className="mt-4 w-full animate-pulse flex-row items-center justify-center space-x-1 rounded-xl ">
+          <div className="flex flex-col space-y-2">
+            <div className="h-6 w-11/12 rounded-md bg-gray-300 "></div>
+            <div className="h-6 w-11/12 rounded-md bg-gray-300 "></div>
+            <div className="h-6 w-11/12 rounded-md bg-gray-300 "></div>
+            <div className="h-6 w-10/12 rounded-md bg-gray-300 "></div>
+            <div className="h-6 w-10/12 rounded-md bg-gray-300 "></div>
+            <div className="h-6 w-10/12 rounded-md bg-gray-300 "></div>
+            <div className="h-6 w-9/12 rounded-md bg-gray-300 "></div>
+            <div className="h-6 w-9/12 rounded-md bg-gray-300 "></div>
+            <div className="h-6 w-9/12 rounded-md bg-gray-300 "></div>
+            <div className="h-6 w-9/12 rounded-md bg-gray-300 "></div>
+            <div className="h-6 w-9/12 rounded-md bg-gray-300 "></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SkeletonCard;
