7091199c39717d0c10377e09f5d90b1ae3f97f13
loegnah <loegnah@gmail.com>
2022-06-21 03:15:56 +0900

    Wonder in posts

---
+++ pages/api/posts/[id]/wonder.ts
@@ -0,0 +1,57 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function wonderHandler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+
+  const alreadyExist = await client.wondering.findFirst({
+    where: {
+      userId: user?.id,
+      postId: +id.toString(),
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  if (alreadyExist) {
+    await client.wondering.delete({
+      where: {
+        id: alreadyExist.id,
+      },
+    });
+  } else {
+    await client.wondering.create({
+      data: {
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+        post: {
+          connect: {
+            id: +id.toString(),
+          },
+        },
+      },
+    });
+  }
+  res.json({
+    ok: true,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["POST"],
+    handler: wonderHandler,
+  })
+);
